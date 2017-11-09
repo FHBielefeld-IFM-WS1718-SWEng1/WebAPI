c232a4d746771daad348bac047ebbc7b4897f61a
Unknown <aheinisch@fh-bielefeld.de>
2017-11-09 16:06:33 +0100

    Only english Var names

--- routes/parties.js
+++ routes/parties.js
@@ -5,11 +5,12 @@ var highestID = 0;
 
 /* POST parties listing. */
 router.post('/', function (req, res, next) {
-    if ("name" in req.body && "age" in req.body && req.body.name && req.body.age) {
+    if ("name" in req.body && "description" in req.body && req.body.name && req.body.description) {
         var entry = {};
         entry.id = highestID++;
         entry.name = req.body.name;
         entry.description = req.body.description;
+        entry.invited = req.body.invited || [];
         temp.values.push(entry);
         res.json(entry);
     } else {
@@ -52,9 +53,20 @@ router.put('/:id', function (req, res, next) {
         var erfolg;
         for (i = 0; i < temp.values.length; i++) {
             if (temp.values[i].id == id) {
+                if("name" in req.body && req.body.name ){
+                    temp.values[i].name = req.body.name;
+                }
+                if("dexcription" in req.body && req.body.description ){
+                    temp.values[i].description = req.body.description;
+                }
+                if("invited" in req.body && req.body.invited ){
+                    temp.values[i].invited = req.body.invited || [];
+                }
+                temp.values.push(entry);
                 res.json(temp.values[id]);
                 erfolg = true;
             }
+
         }
         if (!erfolg) {
             res.json({error: 404, name: "Keine Partie mit der id " + id});
@@ -64,6 +76,7 @@ router.put('/:id', function (req, res, next) {
         res.json({id: "missing", name: "get"});
     }
 });
+;
 
 
 /* DELETE parties listing. */
@@ -75,8 +88,8 @@ router.delete('/:id', function (req, res, next) {
         var erfolg;
         for (i = 0; i < temp.values.length; i++) {
             if (temp.values[i].id == id) {
-                temp.values.splice(i,1);
-                res.json({status:"erfolg", text:"element gelöscht"});
+                temp.values.splice(i, 1);
+                res.json({status: "erfolg", text: "element gelöscht"});
                 erfolg = true;
             }
         }
