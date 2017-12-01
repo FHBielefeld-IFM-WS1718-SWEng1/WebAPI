e9659c7d1bb2272af7801eb6320cceda9c355b61
Alexander Heinisch <aheinisch@fh-bielefeld.de>
2017-12-01 21:18:44 +0100

    Hinzufügen von Task Model

---
+++ server/models/model/Task.js
@@ -0,0 +1,9 @@
+module.exports = (function (sequelize, Sequelize) {
+    return sequelize.define('Task', {
+        id: {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true, field: 'ID'},
+        text: {type: Sequelize.STRING(100), field: 'Text'},
+        status: {type: Sequelize.INTEGER, field: 'Status'},
+        user_id: {type: Sequelize.INTEGER, references: {model: 'User', key: 'ID'}, field: 'User_ID'},
+        party_id: {type: Sequelize.INTEGER, references: {model: 'Party', key: 'ID'}, field: 'Party_ID'}
+    });
+});
