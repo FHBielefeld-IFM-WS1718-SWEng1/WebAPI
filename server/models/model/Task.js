6970c9548439317656ddf95293b82dfeacee51e1
Alexander Heinisch <aheinisch@fh-bielefeld.de>
2017-12-01 21:46:37 +0100

    Hinzufügen von Zeitstempel im Task Model

--- server/models/model/Task.js
+++ server/models/model/Task.js
@@ -4,6 +4,8 @@ module.exports = (function (sequelize, Sequelize) {
         text: {type: Sequelize.STRING(100), field: 'Text'},
         status: {type: Sequelize.INTEGER, field: 'Status'},
         user_id: {type: Sequelize.INTEGER, references: {model: 'User', key: 'ID'}, field: 'User_ID'},
-        party_id: {type: Sequelize.INTEGER, references: {model: 'Party', key: 'ID'}, field: 'Party_ID'}
+        party_id: {type: Sequelize.INTEGER, references: {model: 'Party', key: 'ID'}, field: 'Party_ID'},
+        createdAt: {type: Sequelize.DATE, field: 'CreatedAt'},
+        updatedAt: {type: Sequelize.DATE, field: 'ChangedAt'}
     });
 });
