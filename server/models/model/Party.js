6144aa02217cad4312fa226ecfc3c6c0e9b14402
Alexander Heinisch <aheinisch@fh-bielefeld.de>
2017-12-01 21:22:27 +0100

    Anpassen des Party Models an neues Schema

--- server/models/model/Party.js
+++ server/models/model/Party.js
@@ -1,13 +1,14 @@
 module.exports = (function (sequelize, Sequelize) {
     return sequelize.define('Party', {
-        id: {type: Sequelize.INTEGER, primaryKey: true},
-        name: {type: Sequelize.STRING(50)},
-        description: {type: Sequelize.STRING},
-        startDate: {type: Sequelize.DATE},
-        endDate: {type: Sequelize.DATE},
+        id: {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true, field: 'ID'},
+        name: {type: Sequelize.STRING(50), field: 'Name'},
+        description: {type: Sequelize.TEXT, field: 'Description'},
+        startDate: {type: Sequelize.DATE, field: 'StartDate'},
+        endDate: {type: Sequelize.DATE, field: 'EndDate'},
+        location: {type: Sequelize.String(100), field: 'Location'},
         createdAt: {type: Sequelize.DATE, field: 'CreatedAt'},
         updatedAt: {type: Sequelize.DATE, field: 'ChangedAt'},
         deletedAt: {type: Sequelize.DATE, field: 'DeletedAt'},
-        user_ID: {type: Sequelize.INTEGER, references: {model: 'User', key: 'ID'}}
+        user_ID: {type: Sequelize.INTEGER, references: {model: 'User', key: 'ID'}, field: 'User_ID'}
     });
 });
