1e8b08296439c26341fb9520e92dcc33094cf646
Alexander Heinisch <aheinisch@fh-bielefeld.de>
2017-12-01 21:48:21 +0100

    Hinzufügen von Zeitstempel im Choice Model

--- server/models/model/Choice.js
+++ server/models/model/Choice.js
@@ -2,6 +2,8 @@ module.exports = (function (sequelize, Sequelize) {
     return sequelize.define('Choice', {
         id: {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true, field: 'ID'},
         text: {type:Sequelize.STRING(100), field: 'Text'},
-        voting_id: {type: Sequelize.INTEGER, references: {model: 'Voting', key: 'ID'}, field: 'Voting_ID'}
+        voting_id: {type: Sequelize.INTEGER, references: {model: 'Voting', key: 'ID'}, field: 'Voting_ID'},
+        createdAt: {type: Sequelize.DATE, field: 'CreatedAt'},
+        updatedAt: {type: Sequelize.DATE, field: 'ChangedAt'}
     });
 });
