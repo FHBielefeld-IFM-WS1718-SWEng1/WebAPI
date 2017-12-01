a34576b452d8531cff318267769377d9b62008d3
Unknown <florian.wiese@fh-bielefeld.de>
2017-12-01 13:11:17 +0100

    Login angefangen, party model überarbeitet

--- server/models/model/Party.js
+++ server/models/model/Party.js
@@ -1,10 +1,10 @@
 module.exports = (function (sequelize, Sequelize) {
     return sequelize.define('Party', {
-        ID: {type: Sequelize.INTEGER, primaryKey: true},
-        Name: {type: Sequelize.STRING(50)},
-        Description: {type: Sequelize.STRING},
-        StartDate: {type: Sequelize.DATE},
-        EndDate: {type: Sequelize.DATE},
+        id: {type: Sequelize.INTEGER, primaryKey: true},
+        name: {type: Sequelize.STRING(50)},
+        description: {type: Sequelize.STRING},
+        startDate: {type: Sequelize.DATE},
+        endDate: {type: Sequelize.DATE},
         createdAt: {type: Sequelize.DATE, field: 'CreatedAt'},
         updatedAt: {type: Sequelize.DATE, field: 'ChangedAt'},
         deletedAt: {type: Sequelize.DATE, field: 'DeletedAt'},
