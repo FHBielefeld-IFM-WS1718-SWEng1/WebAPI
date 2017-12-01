c9b2253b9fc38d9b2607ec1720d364803c89f654
Alexander Heinisch <aheinisch@fh-bielefeld.de>
2017-12-01 21:46:19 +0100

    Hinzufügen von Zeitstempel im Todolistitem Model

--- server/models/model/Todolistitem.js
+++ server/models/model/Todolistitem.js
@@ -3,6 +3,8 @@ module.exports = (function (sequelize, Sequelize) {
         id: {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true, field: 'ID'},
         text: {type:Sequelize.TEXT , field: 'Text'},
         status: {type: Sequelize.INTEGER, field: 'Status'},
-        party_id: {type: Sequelize.INTEGER, references: {model: 'Party', key: 'ID'}, field: 'Party_ID'}
+        party_id: {type: Sequelize.INTEGER, references: {model: 'Party', key: 'ID'}, field: 'Party_ID'},
+        createdAt: {type: Sequelize.DATE, field: 'CreatedAt'},
+        updatedAt: {type: Sequelize.DATE, field: 'ChangedAt'}
     });
 });
