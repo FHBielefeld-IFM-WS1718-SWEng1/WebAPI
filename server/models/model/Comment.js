7a953354de26d738ef8312e290aff313c94cb277
Alexander Heinisch <aheinisch@fh-bielefeld.de>
2017-12-01 21:26:01 +0100

    Hinzufügen von Comment Model

---
+++ server/models/model/Comment.js
@@ -0,0 +1,8 @@
+module.exports = (function (sequelize, Sequelize) {
+    return sequelize.define('Comment', {
+        id: {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true, field: 'ID'},
+        text: {type:Sequelize.TEXT, field: 'Text'},
+        user_id: {type: Sequelize.INTEGER, references: {model: 'User', key: 'ID'}, field: 'User_ID'},
+        party_id: {type: Sequelize.INTEGER, references: {model: 'Party', key: 'ID'}, field: 'Party_ID'}
+    });
+});
