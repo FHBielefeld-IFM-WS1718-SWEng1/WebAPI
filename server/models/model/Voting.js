63e18c2f87bb991809ce2c87f44d5f3c009fd1ec
Alexander Heinisch <aheinisch@fh-bielefeld.de>
2017-12-01 21:43:19 +0100

    Hinzufügen von Zeitstempel im Voting Model

--- server/models/model/Voting.js
+++ server/models/model/Voting.js
@@ -3,6 +3,8 @@ module.exports = (function (sequelize, Sequelize) {
         id: {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true, field: 'ID'},
         name: {type: Sequelize.STRING(100), field: 'Name'},
         user_id: {type: Sequelize.INTEGER, references: {model: 'User', key: 'ID'}, field: 'User_ID'},
-        party_id: {type: Sequelize.INTEGER, references: {model: 'Party', key: 'ID'}, field: 'Party_ID'}
+        party_id: {type: Sequelize.INTEGER, references: {model: 'Party', key: 'ID'}, field: 'Party_ID'},
+        createdAt: {type: Sequelize.DATE, field: 'CreatedAt'},
+        updatedAt: {type: Sequelize.DATE, field: 'ChangedAt'}
     });
 });
