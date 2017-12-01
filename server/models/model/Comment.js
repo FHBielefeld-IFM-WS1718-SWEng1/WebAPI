e76db6d1c08337618d79ff67ea83dea074450493
Alexander Heinisch <aheinisch@fh-bielefeld.de>
2017-12-01 21:47:52 +0100

    Hinzufügen von Zeitstempel im Comment Model

--- server/models/model/Comment.js
+++ server/models/model/Comment.js
@@ -3,6 +3,8 @@ module.exports = (function (sequelize, Sequelize) {
         id: {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true, field: 'ID'},
         text: {type:Sequelize.TEXT, field: 'Text'},
         user_id: {type: Sequelize.INTEGER, references: {model: 'User', key: 'ID'}, field: 'User_ID'},
-        party_id: {type: Sequelize.INTEGER, references: {model: 'Party', key: 'ID'}, field: 'Party_ID'}
+        party_id: {type: Sequelize.INTEGER, references: {model: 'Party', key: 'ID'}, field: 'Party_ID'},
+        createdAt: {type: Sequelize.DATE, field: 'CreatedAt'},
+        updatedAt: {type: Sequelize.DATE, field: 'ChangedAt'}
     });
 });
