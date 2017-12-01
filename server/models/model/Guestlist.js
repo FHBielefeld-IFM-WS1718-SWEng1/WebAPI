c0c4566da9762edac8128916c0f22874c9ef2447
Alexander Heinisch <aheinisch@fh-bielefeld.de>
2017-12-01 21:47:31 +0100

    Hinzufügen von Zeitstempel im Guestlist Model

--- server/models/model/Guestlist.js
+++ server/models/model/Guestlist.js
@@ -2,6 +2,8 @@ module.exports = (function (sequelize, Sequelize) {
     return sequelize.define('Guestlist', {
         user_id: {type: Sequelize.INTEGER, references: {model: 'User', key: 'ID'}, field: 'User_ID'},
         party_id: {type: Sequelize.INTEGER, references: {model: 'Party', key: 'ID'}, field: 'Party_ID'},
-        status: {type: Sequelize.INTEGER, field: 'Status'}
+        status: {type: Sequelize.INTEGER, field: 'Status'},
+        createdAt: {type: Sequelize.DATE, field: 'CreatedAt'},
+        updatedAt: {type: Sequelize.DATE, field: 'ChangedAt'}
     });
 });
