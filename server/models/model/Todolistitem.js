10cda80ca5eb019c1b63bf53e385cad8c5671d24
Alexander Heinisch <aheinisch@fh-bielefeld.de>
2017-12-01 21:15:10 +0100

    Hinzufügen von Todolistitem Model

---
+++ server/models/model/Todolistitem.js
@@ -0,0 +1,8 @@
+module.exports = (function (sequelize, Sequelize) {
+    return sequelize.define('Todolistitem', {
+        id: {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true, field: 'ID'},
+        text: {type:Sequelize.TEXT , field: 'Text'},
+        status: {type: Sequelize.INTEGER, field: 'Status'},
+        party_id: {type: Sequelize.INTEGER, references: {model: 'Party', key: 'ID'}, field: 'Party_ID'}
+    });
+});
