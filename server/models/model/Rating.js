070f976753897c62bd19fcedb546694999968cb6
Alexander Heinisch <aheinisch@fh-bielefeld.de>
2017-12-01 21:47:18 +0100

    Hinzufügen von Zeitstempel im Rating Model

--- server/models/model/Rating.js
+++ server/models/model/Rating.js
@@ -3,6 +3,8 @@ module.exports = (function (sequelize, Sequelize) {
         id: {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true, field: 'ID'},
         value: {type: Sequelize.FLOAT, field: 'Value'},
         user_id: {type: Sequelize.INTEGER, references: {model: 'User', key: 'ID'}, field: 'User_ID'},
-        party_id: {type: Sequelize.INTEGER, references: {model: 'Party', key: 'ID'}, field: 'Party_ID'}
+        party_id: {type: Sequelize.INTEGER, references: {model: 'Party', key: 'ID'}, field: 'Party_ID'},
+        createdAt: {type: Sequelize.DATE, field: 'CreatedAt'},
+        updatedAt: {type: Sequelize.DATE, field: 'ChangedAt'}
     });
 });
