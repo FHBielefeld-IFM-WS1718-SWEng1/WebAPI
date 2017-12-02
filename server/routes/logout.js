0000f2af295888a2b3bfeb2207ac842d7e906ee5
Unknown <nloetkemann@fh-bielefeld.de>
2017-12-02 12:07:50 +0100

    Anfang logout

---
+++ server/routes/logout.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+/**
+ * Delete von dem APIKey des Users
+ */
+router.delete('/', function (req , res, next){
+    if("apiKey" in req.body){
+        var key = req.body.apiKey
+        req.models.APIKey.destroy({where: {apiKey: key}}).then(result => {
+            res.status(200);
+        }).catch(err => {
+            res.status(400);
+        })
+    } else {
+        res.status(404)
+    }
+    res.status(400);
+});
