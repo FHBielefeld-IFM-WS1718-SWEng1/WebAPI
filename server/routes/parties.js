c53d635389b9cee6a1177402998fe947a91656e9
Alexander Heinisch <aheinisch@fh-bielefeld.de>
2017-12-03 22:00:18 +0100

    Partie Route überarbeitung! noch lange nicht fertig

--- server/routes/parties.js
+++ server/routes/parties.js
@@ -21,9 +21,69 @@ router.post('/', function (req, res, next) {
     }
 });
 
+
+/*
+    Hier sollen alle Parties angezeigt werden die entweder von User erstellt wurden oder wo er als Gast eingeladen wurde.
+*/
+// TODO Route zum anzeigen aller Parties an welche der User teilnimmt
+router.get('/', function (req, res, next) {
+    console.log('get/ohneid');
+    // nach überlegungen wäre es vielleicht besser wenn das Frontend einfach auch den User mit angibt ... dennoch war in der besprechung gesagt worden, dass das Frontend einfach nur den APIKey schmeist
+    // somit müssen wir den User finden dem der APIKey gehört
+    if ('api' in req.query && req.query.api) {
+        // kontrolle ob der API-Key gültig ist
+        // Außerdem wird so die User_ID gefunden
+        req.models.APIKey.findAll({
+            include: [{model: req.models.User}],
+            where: {apiKey: req.query.api}
+        }).then((results) => {
+            console.log(results[0])
+            console.log();
+            if (results.length === 1) {
+                var array = [];
+                req.models.User.findAll({
+                    where: {id: results[0].user_id}
+                }).then((user) => {
+                    console.log(user);
+                    console.log();
+                });
+                // durchsuchen aller Partys wo der User Gast ist
+                req.models.Guestlist.findAll({where: {user_id: results[0].user_id}}).then((resultsGuest) => {
+                    var array2 = []
+                    resultsGuest.forEach((eintrag) => array2.push(eintrag));
+                    req.models.Party.findAll({
+                        where: {
+                            user_ID: results[0].user_id,
+                            id: array2
+                        }
+                    }).then((resultPartysErsteller) => {
+                        resultPartysErsteller.forEach((eintrag) => array.push(eintrag));
+                        console.log(array2);
+                        console.log();
+                        console.log(array);
+                        console.log();
+                        res.status(200);
+                        res.json(array);
+                    }).catch((err) => next(err));
+                }).catch((err) => next(err));
+            } else if (results.length > 1) {
+                console.log(res.header);
+                var error = new error(400, 'doppelte keys vorhanden eindeutigkeit verloren bitte neuen KeyGenerieren!');
+                next(error);
+            } else {
+                var error = new error(400, 'API Key ungültig!');
+                next(error);
+            }
+        }).catch(err => next(err));
+    } else {
+        res.json({error: "Du scheis node"});
+    }
+});
+
 /* GET parties listing. */
 // TODO Route zum anzeigen einer speziellen Party an welche der User Teilnehmen kann oder mitglied ist!
 router.get('/:id', function (req, res, next) {
+    console.log('get/id');
     if ("id" in req.params && req.params.id) {
         var id = req.params.id;
         var i;
@@ -44,68 +104,6 @@ router.get('/:id', function (req, res, next) {
     }
 });
 
-/*
-    Hier sollen alle Parties angezeigt werden die entweder von User erstellt wurden oder wo er als Gast eingeladen wurde.
-*/
-// TODO Route zum anzeigen aller Parties an welche der User teilnimmt
-router.get('/', function (req, res, next) {
-    // nach überlegungen wäre es vielleicht besser wenn das Frontend einfach auch den User mit angibt ... dennoch war in der besprechung gesagt worden, dass das Frontend einfach nur den APIKey schmeist
-    // somit müssen wir den User finden dem der APIKey gehört
-    if ('key' in req.query && req.query.key) {
-        // kontrolle ob der API-Key gültig ist
-        // Außerdem wird so die User_ID gefunden
-        req.models.ApiKey.findAll({where: {apiKey: req.query.key}}).then((results) => {
-            if (results.length === 1) {
-                var array = [];
-                // durchsuchen aller Partys wo der User Gast ist
-                req.models.Guestlist.findAll({where: {user_id: results[0].user_id}}).then((resultsGuest) => {
-                    var array2 = []
-                    resultsGuest.forEach((eintrag) => array2.push(eintrag));
-                    req.models.Party.findAll({
-                        where: {
-                            user_ID: results[0].user_id,
-                            id: array2
-                        }
-                    }).then((resultPartysErsteller) => {
-                        resultPartysErsteller.forEach((eintrag) => array.push(eintrag));
-                        res.status(200);
-                        res.json(array);
-                    }).catch((err) => next(err));
-                }).catch((err) => next(err));
-            } else if (results.length > 1) {
-                var error = new error(400, 'doppelte keys vorhanden eindeutigkeit verloren bitte neuen KeyGenerieren!');
-                next(error);
-            } else {
-                var error = new error(400, 'API Key ungültig!');
-                next(error);
-            }
-        })
-    }
-    if ("id" in req.params && req.params.id) {
-        var id = req.params.id;
-        req.models.User.findById(id)
-            .then((user) => {
-                if (user) {
-                    delete user.dataValues.password;
-                    res.status(200);
-                    res.json(user);
-                } else {
-                    res.status(400);
-                    res.json({error: "Kein User mit der ID " + id});
-                }
-            }).catch((error) => {
-                next(error);
-            }
-        );
-    } else {
-        res.status(404);
-        res.json({id: "missing", name: "get"});
-    }
-
-
-    res.json(temp);
-});
-
 /* PUT parties listing. */
 // TODO Route zum abändern
 router.put('/:id', function (req, res, next) {
@@ -124,7 +122,7 @@ router.put('/:id', function (req, res, next) {
                 if ("invited" in req.body && req.body.invited) {
                     temp.values[i].invited = req.body.invited || [];
                 }
-                res.status = 200
+                res.status = 200;
                 res.json(temp.values[i]);
                 erfolg = true;
             }
@@ -145,7 +143,7 @@ router.delete('/:id', function (req, res, next) {
         var i;
         var erfolg;
         for (i = 0; i < temp.values.length; i++) {
-            if (temp.values[i].id == id) {
+            if (temp.values[i].id === id) {
                 temp.values.splice(i, 1);
                 res.json({status: "erfolg", text: "element gelöscht"});
                 erfolg = true;
