afaf7096822dea42571995b3b37f973e6a88eea3
Alexander Heinisch <aheinisch@fh-bielefeld.de>
2017-12-03 21:58:30 +0100

    Partie Route Testfall!
    SEHR EXPETIMENTELL!!!!!!

--- test/functions/test.js
+++ test/functions/test.js
@@ -7,3 +7,41 @@ describe('Parameterisierte', () => {
         });
     });
 });
+describe('Fuckyoutests', () => {
+    it('fickdich', (done) => {
+        chai.request(server)
+            .post('/register')
+            .send({
+                "name": "Peter",
+                "password": "fisch",
+                "email": "demo@demo.de"
+            })
+            .end(function (err, res) {
+                expect(res).to.have.status(200);
+                expect(res).to.be.json;
+                object = res.body;
+                done();
+                chai.request(server)
+                    .post('/login')
+                    .send({
+                        "name": "Peter",
+                        "password": "fisch",
+                        "email": "demo@demo.de"
+                    })
+                    .end(function (err, res1) {
+                        expect(res).to.have.status(200);
+                        expect(res).to.be.json;
+                        object = res1.body
+                        done();
+                        chai.request(server)
+                            .get('/parties?api=' + res1.body.key)
+                            .end(function (err, res) {
+                                expect(res).to.have.status(200);
+                                expect(res).to.be.json;
+                                object = res.body
+                                done();
+                            });
+                    });
+            });
+    });
+});
