b2eb895910f396873e4d17ed6e2db3d519066cd0
Alexander Heinisch <aheinisch@fh-bielefeld.de>
2017-12-03 11:08:08 +0100

    Hinzufügen von Primitiven Tests für login

---
+++ test/routes/login-test.js
@@ -0,0 +1,51 @@
+describe('login', () => {
+    describe('POST', () => {
+        it('es ist erlaubt sich einzuloggen', (done) => {
+            chai.request(server)
+                .post('/login')
+                .send({email: "fisch@fisch.de", password: "strenggeheim"})
+                .end(function (err, res) {
+                    expect(res).to.have.status(200);
+                    expect(res).to.be.json;
+                    object = res.body
+                    done();
+                });
+        });
+    });
+    describe('GET', () => {
+        it('es ist nicht erlaubt einen GET auszuführen', (done) => {
+            chai.request(server)
+                .get('/login')
+                .end(function (err, res) {
+                    expect(res).to.have.status(404);
+                    expect(res).to.be.json;
+                    object = res.body
+                    done();
+                });
+        });
+    });
+    describe('PUT', () => {
+        it('es ist nicht erlaubt einen PUT auszuführen', (done) => {
+            chai.request(server)
+                .put('/login')
+                .end(function (err, res) {
+                    expect(res).to.have.status(404);
+                    expect(res).to.be.json;
+                    object = res.body
+                    done();
+                });
+        });
+    });
+    describe('DELETE', () => {
+        it('es ist nicht erlaubt einen DELETE auszuführen', (done) => {
+            chai.request(server)
+                .delete('/login')
+                .end(function (err, res) {
+                    expect(res).to.have.status(404);
+                    expect(res).to.be.json;
+                    object = res.body
+                    done();
+                });
+        });
+    });
+});
