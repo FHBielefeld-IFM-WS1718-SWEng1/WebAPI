02b9d318f366e6b9c17eb14fb68f2560fed25386
Alexander Heinisch <aheinisch@fh-bielefeld.de>
2017-12-05 13:20:19 +0100

    Verbessern der Register tests

--- test/routes/register-test.js
+++ test/routes/register-test.js
@@ -2,12 +2,12 @@ describe('register', () => {
     describe('POST', () => {
         it('es ist erlaubt einen neuen Nutzer anzulegen', (done) => {
             chai.request(server)
-                .post('/logout')
-                .send({email: "fisch@fisch.de", password: "strenggeheim"})
+                .post('/register')
+                .send({name: "Peter", email: "fisch@fisch.de", password: "strenggeheim"})
                 .end(function (err, res) {
                     expect(res).to.have.status(200);
                     expect(res).to.be.json;
-                    object = res.body
+                    object = res.body;
                     done();
                 });
         });
@@ -15,11 +15,11 @@ describe('register', () => {
     describe('GET', () => {
         it('es ist nicht erlaubt einen GET auszuführen', (done) => {
             chai.request(server)
-                .get('/logout')
+                .get('/register')
                 .end(function (err, res) {
                     expect(res).to.have.status(404);
                     expect(res).to.be.json;
-                    object = res.body
+                    object = res.body;
                     done();
                 });
         });
@@ -27,11 +27,12 @@ describe('register', () => {
     describe('PUT', () => {
         it('es ist nicht erlaubt einen PUT auszuführen', (done) => {
             chai.request(server)
-                .put('/logout')
+                .put('/register')
+                .send()
                 .end(function (err, res) {
                     expect(res).to.have.status(404);
                     expect(res).to.be.json;
-                    object = res.body
+                    object = res.body;
                     done();
                 });
         });
@@ -39,11 +40,12 @@ describe('register', () => {
     describe('DELETE', () => {
         it('es ist nicht erlaubt einen DELETE auszuführen', (done) => {
             chai.request(server)
-                .delete('/logout')
+                .delete('/register')
+                .send()
                 .end(function (err, res) {
                     expect(res).to.have.status(404);
-                    expect(res).to.be.json;
-                    object = res.body
+                    expect(res).to.be.json
+                    object = res.body;
                     done();
                 });
         });
