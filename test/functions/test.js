1d1f661611709e158be0810694ccd18a27646852
Alexander Heinisch <aheinisch@fh-bielefeld.de>
2017-12-06 21:28:06 +0100

    entfernen von beispiel für parameterisierte Tests

--- test/functions/test.js
+++
@@ -1,9 +0,0 @@
-describe('Parameterisierte', () => {
-    describe('test ', () => {
-        [1, 2, 3, 4].forEach((value) => {
-            it('bla ' + value, function (done) {
-                done();
-            });
-        });
-    });
-});
