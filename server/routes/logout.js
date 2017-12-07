a7b62ca4fe256c7258958944ea5fc4d24e71173b
Alexander Heinisch <aheinisch@fh-bielefeld.de>
2017-12-07 16:42:03 +0100

    Wirklicher Fix

--- server/routes/logout.js
+++ server/routes/logout.js
@@ -4,22 +4,17 @@ const router = express.Router();
 /**
  * Delete von dem APIKey des Users
  */
-router.delete('/', function (req, res, next) {
-    // TODO der Key steht wie bei jedem Request vorerst im Query somit nicht im body
-    if ("apiKey" in req.body) {
-        // TODO hier wird auch reingegangen wenn kein gültiger apiKey übergeben wird z.b. delete auf 'localhost:8080/logout?api='
-        var key = req.body.apiKey;
+router.delete('/', function (req , res, next){
+    if( req.params.apiKey && "apiKey" in req.params){
+        var key = req.body.apiKey
         req.models.APIKey.destroy({where: {apiKey: key}}).then(result => {
             res.status(200);
         }).catch(err => {
-            // TODO Fehlender Response außerdem wäre hier ein next(err) besser da der Error handler dadurch aufgerufen wird und bessere Fehlermeldungen liefert da dies ein 5XX error ist
-            res.status(400);
-        });
+            next(err);
+        })
     } else {
-        // TODO Fehlender res.json
-        res.status(404);
+        res.status(404)
+        res.json({id:"missing", name:"apiKey"});
     }
-    // TODO Fehlender res.json außerdem Doppelter Response
-    res.status(400);
 });
 module.exports = router;
