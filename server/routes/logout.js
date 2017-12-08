2f7b7eb24e4fb64a967bad6e94f1316d8e0f8a6c
Unknown <nloetkemann@fh-bielefeld.de>
2017-12-08 10:42:34 +0100

    fix no2

--- server/routes/logout.js
+++ server/routes/logout.js
@@ -5,16 +5,11 @@ const router = express.Router();
  * Delete von dem APIKey des Users
  */
 router.delete('/', function (req , res, next){
-    if( req.params.apiKey && "apiKey" in req.params){
-        var key = req.body.apiKey
-        req.models.APIKey.destroy({where: {apiKey: key}}).then(result => {
-            res.status(200);
-        }).catch(err => {
-            next(err);
-        })
-    } else {
-        res.status(404)
-        res.json({id:"missing", name:"apiKey"});
-    }
+    var key = req.apiKey;
+    req.models.APIKey.destroy({where: {apiKey: key}}).then(result => {
+        res.status(200);
+        res.json({status:"Erfolg"});
+    }).catch(err => {
+        next(err);
+    })
 });
-module.exports = router;
