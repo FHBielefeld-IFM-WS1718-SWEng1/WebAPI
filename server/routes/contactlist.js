2eadab9d0e4a50841c5ff13edeb59923e5ac1cd4
Alexander Heinisch <aheinisch@fh-bielefeld.de>
2017-12-13 10:52:01 +0100

    einrichten von DELETE /user/contact

--- server/routes/contactlist.js
+++ server/routes/contactlist.js
@@ -24,24 +24,69 @@ router.get('/', (req, res, next) => {
             model: req.models.User
         }
     }).then((result) => {
-        var ret = {count: 0, contacts: []};
+        let ret = {count: 0, contacts: []};
         if (result) {
 
-            result.forEach(val=>{
+            result.forEach(val => {
                 console.log(val);
                 delete val.User.dataValues.password;
                 delete val.User.dataValues.createdAt;
                 delete val.User.dataValues.updatedAt;
                 delete val.User.dataValues.deletedAt;
-                ret.contacts.push(val.User.dataValues)
-
-                console.log("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+                ret.contacts.push(val.User.dataValues);
             });
         }
-        console.log(result);
-        console.log("Hey Party!");
         res.status(200);
         res.json(ret);
-    }).catch(err =>{console.error(err); next(err)});
+    }).catch(err => {
+        console.error(err);
+        next(err)
+    });
+});
+
+router.delete('/', (req, res, next) => {
+
+    req.models.Contactlist.destroy({
+        where:
+            {
+                $or: [
+                    {
+                        $and: [{
+                            user_id1:
+                                {
+                                    $eq: req.userid
+                                }
+                        },{
+                            user_id2:
+                                {
+                                    $eq: req.body.id
+                                }
+                        }]
+                    },
+                    {
+                        $and: [{
+                            user_id1:
+                                {
+                                    $eq: req.body.userid
+                                }
+                        },{
+                            user_id2:
+                                {
+                                    $eq: req.userid
+                                }
+                        }]
+                    }
+                ]
+            },
+        include: {
+            model: req.models.User
+        }
+    }).then((result) => {
+        res.status(200);
+        res.json({message: "erfolg", items: result});
+    }).catch(err => {
+        console.error(err);
+        next(err)
+    });
 });
 module.exports = router;
