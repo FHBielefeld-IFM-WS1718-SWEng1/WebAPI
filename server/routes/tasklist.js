8c64621620dc4e8c27a167131f2daf7787f9deb0
Alexander Heinisch <aheinisch@fh-bielefeld.de>
2017-12-15 18:59:35 +0100

    Route POST /party/task angelegt
    Grundimplementation

--- server/routes/tasklist.js
+++ server/routes/tasklist.js
@@ -3,5 +3,21 @@ const router = express.Router();
 const util = require('../auth/utilities');
 
 
+router.post('/', (req, res, next) => {
+    req.models.create({
+        user_id: req.body.user_id,
+        party_id: req.body.party_id,
+        text: req.body.text,
+        status: req.body.status
+    }).then(value => {
+        if (value) {
+            res.status(200);
+            res.json(value);
+        } else {
+            next({status: 400, message: "Kein Datensatz wurde angelegt"});
+        }
+    }).catch(err => next(err));
+
+});
 
 module.exports = router;
