32d48597813e96193857134c066fbc022d6f671f
Alexander Heinisch <aheinisch@fh-bielefeld.de>
2018-01-10 17:00:01 +0100

    Bilder upload post get abgeschlossen

--- server/routes/image.js
+++ server/routes/image.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const router = express.Router();
 const util = require('../helper/utilities');
+const crypt = require('../helper/crypt');
 const fs = require('fs');
 router.post('/', (req, res, next) => {
-    fs.writeFile("./demo/1.jpg", req.body.data,'base64' , next);
-    //res.status(200);
-    //res.json({works:"works"});
-});
 
+    let file = crypt.encurl("" + new Date());
+    console.log(file);
+    fs.writeFileSync("./demo/" + file + ".jpg", req.body.data, 'base64', err => {
+        next(err)
+    });
+    console.log("\n\nwhy\n\n");
+    if (fs.existsSync("./demo/" + file + ".jpg")) {
+        res.status(203);
+        res.json(result);
+    }
+});
+router.get('/:id', (req, res, next) => {
+    if (fs.existsSync("./demo/" + req.body.data + ".jpg")) {
+        res.json({data: fs.readFileSync("./demo/" + req.body.data + ".jpg", 'base64')});
+    } else {
+        next({status:404, message: "Kein Bild mit dieser ID!"});
+    }
+});
 
 module.exports = router;
