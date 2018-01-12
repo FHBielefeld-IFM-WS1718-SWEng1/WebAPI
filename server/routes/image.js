dd03bc98d0669278a6fd0571908e612b637a16b0
Alexander Heinisch <aheinisch@fh-bielefeld.de>
2018-01-12 08:05:45 +0100

    Ordner angepasst

--- server/routes/image.js
+++ server/routes/image.js
@@ -6,17 +6,17 @@ const fs = require('fs');
 router.post('/', (req, res, next) => {
 
     let file = crypt.encurl("" + new Date());
-    fs.writeFileSync("./demo/" + file + ".jpg", req.body.data, 'base64', err => {
+    fs.writeFileSync("./upload/" + file + ".jpg", req.body.data, 'base64', err => {
         next(err)
     });
-    if (fs.existsSync("./demo/" + file + ".jpg")) {
+    if (fs.existsSync("./upload/" + file + ".jpg")) {
         res.status(203);
         res.json({filename: file});
     }
 });
 router.get('/:id', (req, res, next) => {
     if (fs.existsSync("./demo/" + req.params.id + ".jpg")) {
-        res.json({data: fs.readFileSync("./demo/" + req.params.id + ".jpg", 'base64')});
+        res.json({data: fs.readFileSync("./upload/" + req.params.id + ".jpg", 'base64')});
     } else {
         next({status: 404, message: "Kein Bild mit dieser ID!"});
     }
