cbcc9a58a80ff399342ee3b8861d67eeb68903e8
Niki <nloetkemann@fh-bielefeld.de>
2018-01-12 11:26:12 +0100

    post comment ist fertig

--- server/routes/comment.js
+++ server/routes/comment.js
@@ -37,3 +37,15 @@ router.delete('/:id', function (req, res, next){
     }
 });
 
+router.post('/', function (req, res, next){
+    if(util.hasKey(req.body,'user_id') && util.hasKey(req.body,'party_id')){
+        req.models.create({text:req.body.text,user_id:req.body.user_id,party_id:req.body.party_id})
+            .then(result=>{
+                res.status(203);
+                res.json(result);
+            }).catch(err => next(err));
+    } else{
+        next({status: 400, id: "missing", name: "ID"});
+    }
+});
+
