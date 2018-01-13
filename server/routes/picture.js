8fa875ef59a3c06f4a55038219650a79a0331445
Niki <nloetkemann@fh-bielefeld.de>
2018-01-13 12:06:16 +0100

    picutre ändern ist möglich

--- server/routes/picture.js
+++ server/routes/picture.js
@@ -17,4 +17,41 @@ router.get('/:id', function (req, res, next){
     }
 });
 
+
+router.put('/:id', function (req, res, next){
+    if(util.hasKey(req.params,"id")){
+        req.models.Picture.findById(req.params.id).then(picture=>{
+            if(picture){
+                util.changeValueIfExists(picture, req.body, "text");
+                util.changeValueIfExists(picture, req.body, "File");
+                picture.save().then(result=>{
+                    res.status(200);
+                    res.json(picture);
+                }).catch(err => next(err));
+            } else{
+                next({status: 400, message: "Kein Element mit dieser ID gefunden!"});
+            }
+        });
+    } else {
+        next({status: 400, message: "Keine ID vorhanden!"});
+    }
+});
+
+
+router.post('/', function (req, res, next){
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
 module.exports = router;
