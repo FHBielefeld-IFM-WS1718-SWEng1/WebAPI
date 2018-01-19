5caf9d2c5e07d8fcdf440763ef738707b3a7e97f
Niki <nloetkemann@fh-bielefeld.de>
2018-01-19 10:07:24 +0100

    so post in picture

--- server/routes/picture.js
+++ server/routes/picture.js
@@ -23,7 +23,7 @@ router.put('/:id', function (req, res, next){
         req.models.Picture.findById(req.params.id).then(picture=>{
             if(picture){
                 util.changeValueIfExists(picture, req.body, "text");
-                util.changeValueIfExists(picture, req.body, "File");
+                util.changeValueIfExists(picture, req.body, "file");
                 picture.save().then(result=>{
                     res.status(200);
                     res.json(picture);
@@ -42,16 +42,30 @@ router.delete('/:id', function (req, res, next){
     if(util.hasKey(req.params,"id")){
         req.models.Picture.findById(req.params.id).then(picture=>{
             if(picture){
-                picture.destroy().then(r=> picture.save.then(result=>{
+                picture.destroy().then(r=> picture.save().then(result=>{
                     res.status(200);
                     res.json({message:"Erfolg"});
-                }));
+                }).catch(err=>next(err))).catch(err=>next(err));
             } else {
                 next({status: 400, message: "Kein Element mit dieser ID gefunden!"});
             }
         });
     } else {
         next({status: 400, message: "Keine ID vorhanden!"});
+    }
+});
+
+router.post('/', function(req, res, next){
+    if(util.hasKey(req.body,"user_id") && util.hasKey(req.body,"party_id")){
+        req.models.Picture.create({
+            text:req.body.text,
+            file:req.body.file,
+            user_id:req.body.user_id,
+            party_id:req.body.party_id
+        }).then(result=>{
+            res.status(200);
+            res.json(result);
+        }).catch(err=>next(err));
     }
 });
 
