138ef633eb352250e565f0a8fd3020daac8e9734
Alexander Heinisch <aheinisch@fh-bielefeld.de>
2018-01-22 17:07:57 +0100

    gallery einbinden

--- server/app.js
+++ server/app.js
@@ -77,10 +77,9 @@ app.use('/party/rating', rating);
 app.use('/party/todo', todolist);
 app.use('/party/task', tasklist);
 app.use('/party/comment', comment);
+app.use('/party/gallery', gallery);
 app.use('/party/vote', voting);
 app.use('/party', parties);
-app.use('/comment', comment);
-app.use('/gallery',gallery);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
     next({status: 404, message: 'Not Found'});
